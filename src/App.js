19a36f004db234d9f7bf61d7fb16b9c4bdc6c074
Naruepanart Siangsanan <naruepanartwork@gmail.com>
2019-10-09 18:36:06 +0700

    rename actions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import React from "react";
-import { plusCount, minusCount, resetCount } from "./redux/actions";
+import { PLUS_COUNT, MINUS_COUNT, RESET_COUNT } from "./redux/actions";
 
 const Counter = () => {
   const count = useSelector(state => state.count);
@@ -9,9 +9,9 @@ const Counter = () => {
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => dispatch(plusCount())}>PLUS </button>
-      <button onClick={() => dispatch(minusCount())}>MINUS</button>
-      <button onClick={() => dispatch(resetCount())}>RESET</button>
+      <button onClick={() => dispatch(PLUS_COUNT())}>PLUS </button>
+      <button onClick={() => dispatch(MINUS_COUNT())}>MINUS</button>
+      <button onClick={() => dispatch(RESET_COUNT())}>RESET</button>
     </div>
   );
 };
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,20 +1,21 @@
-export const plusCount = () => {
+export const PLUS_COUNT = () => {
   return {
     type: "PLUS_COUNT",
     payload: 10
   };
 };
 
-export const minusCount = () => {
+export const MINUS_COUNT = () => {
   return {
     type: "MINUS_COUNT",
     payload: 10
   };
 };
 
-export const resetCount = () => {
+export const RESET_COUNT = () => {
   return {
     type: "RESET_COUNT",
     payload: 100
   };
 };
+
