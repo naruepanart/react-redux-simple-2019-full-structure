a0c6c7cc02b85ba29e8243d9cca2c6d280960398
Naruepanart Siangsanan <naruepanart@users.noreply.github.com>
2020-04-27 09:24:42 +0700

    Update counter.js

--- src/redux/ducks/counter.js
+++ src/redux/ducks/counter.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+// constants
+const PLUS_COUNT = "PLUS_COUNT";
+const MINUS_COUNT = "MINUS_COUNT";
+const RESET_COUNT = "RESET_COUNT";
+const FETCH_USERS = "FETCH_USERS";
+
+// reducer
 const initialState = {
   count: 100,
   fetchUsers: [],
@@ -7,23 +14,24 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case "PLUS_COUNT":
+    case PLUS_COUNT:
       return { ...state, count: state.count + action.payload };
-    case "MINUS_COUNT":
+    case MINUS_COUNT:
       return { ...state, count: state.count - action.payload };
-    case "RESET_COUNT":
+    case RESET_COUNT:
       return { ...state, count: action.payload };
-    case "FETCH_USERS":
+    case FETCH_USERS:
       return { ...state, fetchUsers: action.payload };
     default:
       return state;
   }
 };
 
-export const plus_count = () => ({ type: "PLUS_COUNT", payload: 10 });
-export const mimus_count = () => ({ type: "MINUS_COUNT", payload: 10 });
-export const reset_count = () => ({ type: "RESET_COUNT", payload: 0 });
+// actions
+export const plus_count = () => ({ type: PLUS_COUNT, payload: 10 });
+export const mimus_count = () => ({ type: MINUS_COUNT, payload: 10 });
+export const reset_count = () => ({ type: RESET_COUNT, payload: 0 });
 export const fetch_users = () => async (dispatch) => {
   const result = await axios.get("https://jsonplaceholder.typicode.com/users");
-  dispatch({ type: "FETCH_USERS", payload: result.data });
+  dispatch({ type: FETCH_USERS, payload: result.data });
 };
